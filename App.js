76fbb9731f95720adab8767c8aa2015285b14cfb
hachanghyun <haccwx@naver.com>
2023-08-16 12:27:16 +0900

    url 변경후 commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { WebView } from 'react-native-webview';
 export default function App() {
   return (
     <WebView
-      source={{ uri: 'https://chatgptlotto.pages.dev/' }} 
+      source={{ uri: 'https://ailottoanalyst.pages.dev' }} 
       style={{ margin: 0  ,flex: 1, marginLeft: -105, marginRight:-105
   
       }}
